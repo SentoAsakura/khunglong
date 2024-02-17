54f82857a9b4133ba1983fe8d2d37f0bcc5346e4
Sento Asakura <zenohakai@gmail.com>
2024-02-17 19:31:45 +0700

    Warcrime

--- Commands/test.js
+++ Commands/test.js
@@ -1,6 +1,44 @@
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus, NoSubscriberBehavior, StreamType } = require('@discordjs/voice')
+const player = createAudioPlayer()
+
 module.exports = {
-    name: 'test',
-    execute(message,args){
-        message.channel.send('Bruh')
+    name: 'play',
+    aliases: ['p'],
+    async execute(client,message,args,cmd){
+        const voice_channel = message.member.voice.channel;
+        if (!voice_channel) return message.channel.send('Chú cần vào 1 channel để dùng lệnh này')
+        const permissions = voice_channel.permissionsFor(message.client.user)
+        if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) return message.channel.send('Pri không có quyền đấy')
+
+        player.on('connectionCreate', (queue) => {
+            queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+              const oldNetworking = Reflect.get(oldState, 'networking');
+              const newNetworking = Reflect.get(newState, 'networking');
+        
+              const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+                const newUdp = Reflect.get(newNetworkState, 'udp');
+                clearInterval(newUdp?.keepAliveInterval);
+              }
+        
+              oldNetworking?.off('stateChange', networkStateChangeHandler);
+              newNetworking?.on('stateChange', networkStateChangeHandler);
+            });
+        });
+
+        await client.distube.voices.join(voice_channel)
+        let x = args.join(' ')
+        if(!x){
+            message.channel.send('K.Y.S')
+        }
+        
+        try{
+            client.distube.play(voice_channel, x)
+        } catch(err){
+            console.log(err)
+        }
+        
+        
+        
+        
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const keep = require('./keep')
 const fs = require('fs')
 const Discord = require('discord.js');
 const client = new Discord.Client({intents: 32767});
 const config = require('./Utility/setting.json');
 const PREFIX = config.PREFIX
+const { DisTube } = require('distube')
+const { YtDlpPlugin } = require('@distube/yt-dlp')
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
@@ -12,17 +13,27 @@ for(const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
+client.distube = new DisTube(client, {
+  leaveOnStop: false,
+  emitNewSongOnly: true,
+  emptyCooldown: 300,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false
+})
+
 client.on("messageCreate", message =>{
+    if(message.content === 'Với kho báu này, ta triệu hồi'){
+      message.reply('Có con cặc')
+    }
     if(!message.content.startsWith(PREFIX) || message.author.bot) return;  
     const args = message.content.slice(PREFIX.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd))
     if (!command) return;
-    if(command) command.execute(message, args, cmd)
+    if(command) command.execute(client,message, args, cmd)
 })
 client.on('ready', ()=>{
-    console.log('Contigerncy code: 000 \n Activate')
-    keep()
+  console.log('With this treasure, I summon \n Eight-Handled Sword Divergent Sila Divine General Mahoraga')
 })
   
 const { generateDependencyReport } = require('@discordjs/voice');
