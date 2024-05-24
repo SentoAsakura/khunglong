0e13ca14154bd1d54377eb771cab70e6dbffb200
Sento Asakura <zenohakai@gmail.com>
2024-05-24 19:42:17 +0700

    a

--- Commands/shuffle.js
+++ Commands/shuffle.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'shuffle',
-    aliases:'sf',
+    aliases:['sf'],
     execute(client,message){
         client.distube.shuffle(message)
         message.channel.send('Đã thay đổi thứ tự ưu tiên của các thuật thức')
