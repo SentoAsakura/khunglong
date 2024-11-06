b03fd7f042c42d1fac5299601e50ee12812487bc
Sento Asakura <zenohakai@gmail.com>
2024-11-06 18:24:52 +0700

    Idk

--- Commands/play.js
+++ Commands/play.js
@@ -10,21 +10,6 @@ module.exports = {
         const permissions = voice_channel.permissionsFor(message.client.user)
         if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) return message.channel.send('Pri không có quyền đấy')
 
-        player.on('connectionCreate', (queue) => {
-            queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
-              const oldNetworking = Reflect.get(oldState, 'networking');
-              const newNetworking = Reflect.get(newState, 'networking');
-        
-              const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
-                const newUdp = Reflect.get(newNetworkState, 'udp');
-                clearInterval(newUdp?.keepAliveInterval);
-              }
-        
-              oldNetworking?.off('stateChange', networkStateChangeHandler);
-              newNetworking?.on('stateChange', networkStateChangeHandler);
-            });
-        });
-
         client.distube.voices.join(voice_channel)
         let x = args.join(' ')
         if(!x){
